5211676aa3347eec0e49be3c5221fb637450d00f
Alesja Ersoy <116649067+AlesjaScha@users.noreply.github.com>
2022-12-27 23:20:45 +0100

    Update Picture.js

--- src/Picture.js
+++ src/Picture.js
@@ -5,7 +5,7 @@ const Picture = ()=>{
         </div>
         <div className="photo">
             
-            <img className="image" src="https://img.freepik.com/free-photo/pretty-woman-with-golden-nails-beautiful-gold-jewelry-isolated-white-background_186202-7173.jpg?w=1380&t=st=1671743008~exp=1671743608~hmac=59fd06af3c5bf3b9c40f2aef13b0f7a2ebb5998d506c57334c42b39a0c5f7ca3"/>
+            <img className="image" src="https://img.freepik.com/free-photo/pretty-woman-with-golden-nails-beautiful-gold-jewelry-isolated-white-background_186202-7173.jpg?w=1380&t=st=1671743008~exp=1671743608~hmac=59fd06af3c5bf3b9c40f2aef13b0f7a2ebb5998d506c57334c42b39a0c5f7ca3"alt="bildVier"/>
 
         </div>
         </div>
